68125b322f22e7fd3ddc524f3fa9f9fbb5c183b2
jackbradwilliams <jackbradwilliams@gmail.com>
2022-04-07 13:43:23 -0500

    Added comments to outline files

--- assets/script/data-functions.js
+++ assets/script/data-functions.js
@@ -5,78 +5,94 @@ let picks = JSON.parse(localStorage.getItem("picks")) || [];
 let watched = JSON.parse(localStorage.getItem("watched")) || [];
 // this is the currently displayed movie object
 let current = null;
-const a = [4,2,1]
-const test = [4,3,7,6,44]
+const a = [4, 2, 1];
+const test = [4, 3, 7, 6, 44];
 
+// const that retrieves a random movie thru API call.
+// Returns movie object:
+// (Title, Year, Rated, Genre, Runtime, IMDB rating, Plot, Director, Writer, Director)
+const getRandomMovie = async () => {
+	const rResp = await fetch(
+		"https://k2maan-moviehut.herokuapp.com/api/random"
+	);
+	const rData = await rResp.json();
+	movieTitle = rData.name.toLowerCase();
+	movieTitle.replace(" ", "_");
+	// this gets more details about the movie
+	const response = await fetch(baseURL + movieTitle);
+	const data = await response.json();
+	if (!data.Title) {
+		await getRandomMovie();
+	} else {
+		current = data;
+		current.Runtime = current.Runtime.split(" ")[0];
+	}
+	return current;
+};
 
-const getRandomMovie = async ()=>{
-    // this gets the random movie
-    const rResp = await fetch("https://k2maan-moviehut.herokuapp.com/api/random");
-    const rData = await rResp.json();
-    movieTitle = rData.name.toLowerCase()
-    movieTitle.replace(" ","_")
-    // this gets more details about the movie
-    const response = await fetch(baseURL+movieTitle)
-    const data = await response.json();
-    if(!data.Title){
-        await getRandomMovie()
-    }
-    else {
-    current = data;
-    current.Runtime = current.Runtime.split(" ")[0]
-    }
-    return current;
+// uses "current" object from getRandomMovie()
+// and adds that movie to the queue list
+const addMovieToPicks = () => {
+	current.pickedDate = new Date().getTime();
+	picks.push(current);
+	localStorage.setItem("picks", JSON.stringify(picks));
+	getRandomMovie();
+};
+
+// Sorts queue list:
+// properties can be Year, Title, imdbRating, MetaScore descending is boolean
+const sortMovies = (property, descending, arr) => {
+	if (descending) {
+		arr.sort((a, b) => {
+			return a[property] > b[property] ? -1 : 1;
+		});
+	} else {
+		arr.sort((a, b) => {
+			return a[property] > b[property] ? 1 : -1;
+		});
+	}
+};
+
+// Takes in object from pullVals(), from pull-search-values.js
+// returns array with each property selected
+// ex: genres (Action, comedy, etc.)
+function filterMovies(property, string, array) {
+	const filteredPicks = array.filter((pick) => {
+		return pick[property].includes(string);
+	});
+	return filteredPicks;
 }
 
-const addMovieToPicks = ()=>{
-    current.pickedDate = new Date().getTime();
-    picks.push(current);
-    localStorage.setItem("picks",JSON.stringify(picks));
-    getRandomMovie();
+// See previous function
+function strictFilterMovies(property, string, array) {
+	const filteredPicks = array.filter((pick) => {
+		return pick[property] === string;
+	});
+	return filteredPicks;
 }
 
-// properties can be Year, Title, imdbRating, MetaScore descending is boolean;
-const sortMovies = (property,descending,arr)=>{
-    if (descending){
-        arr.sort((a, b) => {
-             return a[property]>b[property] ? -1 : 1})
-    }
-    else{arr.sort((a, b) => {
-         return a[property]>b[property] ? 1 : -1})};
-}
+// Used for search object in upper left
+const fullText = (obj) => {
+	let text = "";
+	for (let prop in obj) {
+		text += obj[prop];
+	}
+	return text;
+};
 
-function filterMovies(property,string,array){
-    const filteredPicks = array.filter(pick=>{
-        return pick[property].includes(string)
-    }) 
-    return filteredPicks;
-}
+// Takes array of movies
+// Returns array of movies with searched text
+const fullSearch = (string) => {
+	const filteredPicks = picks.filter((pick) => {
+		return fullText(pick).includes(string);
+	});
+	return filteredPicks;
+};
 
-function strictFilterMovies(property,string,array){
-    const filteredPicks = array.filter(pick=>{
-        return pick[property] === string;
-    }) 
-    return filteredPicks;
-}
-
-const fullText = (obj)=>{
-    let text = ""
-    for (let prop in obj){
-        text+=obj[prop];
-    }
-    return text;
-}
-
-const fullSearch = string=>{
-    const filteredPicks = picks.filter((pick)=>{
-        return fullText(pick).includes(string);
-    })
-    return (filteredPicks)    
-}
-
-const removeMovie=(title)=>{
-    console.log(title)
-    picks = picks.filter(pick=>pick.Title != title);
-    localStorage.setItem("picks",JSON.stringify(picks))
-    return picks;
-}
+// Remove from queue list
+const removeMovie = (title) => {
+	console.log(title);
+	picks = picks.filter((pick) => pick.Title != title);
+	localStorage.setItem("picks", JSON.stringify(picks));
+	return picks;
+};
